78f49b0760144effddd2bb60b638330ce26482bc
Minxrod <minxrod@gmail.com>
2022-10-14 15:17:57 -0400

    Version is now specified in the build script; if omitted, uses current time

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@ echo "Creating and building tests file"
 cat test.asm graphic.asm > test_full.asm
 spasm -E -T test_full.asm bin/TEST.8xp
 echo Building tetrice_dat.asm
-spasm -E -T tetrice_dat.asm bin/CETRISDT.8xp 
-spasm -E -L tetrice_dat.asm
+# version information
+cat tetrice_dat.asm > cetrisdt.asm
+echo -e "versionString:\n .db \""${1:-$(date -u +%H:%M)}"\",0\nversionStringEnd:\nversionStringSize = versionStringEnd-versionString" >> cetrisdt.asm
+spasm -E -T cetrisdt.asm bin/CETRISDT.8xp
+spasm -E -L cetrisdt.asm
 tools/convbin -j 8x -k 8xv -i bin/CETRISDT.8xp -o bin/CETrisDT.8xv -n CETrisDT
 echo Creating and building CETRIS
 cat tetrice.asm graphic.asm > cetris.asm
